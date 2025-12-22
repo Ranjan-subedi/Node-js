7cd0d144b22323c7c14621088269e44fe66e8016
Ranjan Subedi <144402172+Ranjan-subedi@users.noreply.github.com>
2025-12-22 18:56:20 +0545

    node

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://ranjansubedi:ranjansubedi@cluster0.ud6gaz6.mongodb.net/?appName=Cluster0").then(function(){
+    
+    // Home route (/)
+    app.get("/",function(req,res){
+        res.send("Hello World done mongoose connection");
+    });
+    
+    app.get("/homepage",function(req,res){
+        res.send("Homepage section");
+    });
+    
+    app.get("/about",function(req,res){
+        res.send("About section");
+    }); 
+    
+    app.get("/contact",function(req,res){
+        req.send("Frrom contact section of the website")
+    });
+
+});
+
+
+
+app.listen(4000,function(){
+    console.log("server is running on port 4000");
+});
