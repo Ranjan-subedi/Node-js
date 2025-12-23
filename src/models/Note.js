d472fb0166be857443e864ccc127273f4a22609a
Ranjan Subedi <144402172+Ranjan-subedi@users.noreply.github.com>
2025-12-23 19:57:22 +0545

    model

---
+++ src/models/Note.js
@@ -0,0 +1,10 @@
+// Steps for defining a model
+
+// 1. Define the schema like this: for a 
+        // Note :  userid
+        //  id , title, content, createdAt, updatedAt
+
+
+// 2.Create a model using the schema <Model Name> <Schema> Note
+
+const mongoose = require('mongoose');
